1eb53e1b0d6cb0932182b6ea5dd0aeee6f9b364e
Jonathan Boada <jonathanboada1@gmail.com>
2024-05-06 13:40:50 -0500

    ajuste

--- includes/app.php
+++ includes/app.php
@@ -1,7 +1,8 @@
 <?php 
+use Dotenv\Dotenv;
 use Model\ActiveRecord;
 require __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 require 'funciones.php';
